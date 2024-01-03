fad277a0457e46396cd5c55cc0e7455863466755
Naman Pandey <2492004namanpandey@gmail.com>
2024-01-03 18:17:02 +0530

    chat container setup

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { allUserRoutes } from '../utils/APIRoutes';
 
 const Chat = () => {
+
+    const [contacts, setContacts] = useState([]);
+    const [currentUser, setCurrentUser] = useState(undefined);
+    const navigate = useNavigate();
+
+    useEffect(async () => {
+        if (!localStorage.getItem('chat-app-user')) {
+            navigate('/login');
+        }else{
+            setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+        }
+    }, [])
+
+    useEffect(async () => {
+        if(currentUser){
+            if(currentUser.isAvatarImageSet){
+                const data = await axios.get(`${allUserRoutes}/${currentUser._id}`);
+                setContacts(data.data);
+            }else{
+                navigate("/setAvatar");
+            }
+        }
+    }, [])
+
     return (
-        <div>
-            Chat
-        </div>
+        <>
+            <Container>
+                <div className="container">
+
+                </div>
+            </Container>
+        </>
     )
 }
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-color: #131324;
+    .container {
+        height: 85vh;
+        width: 85vw;
+        background-color: #00000076;
+        display: grid;
+        grid-template-columns: 25% 75%;
+        @media screeen and (min-width: 720px) and (max-width: 1080px);
+        grid-template-columns: 35% 65%;
+    }
+`;
+
 
 export default Chat
